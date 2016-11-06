8d31d69096ab3ce4daab1fa0ab996fde1bec60d6
bamibestelt <dcmmix@gmail.com>
2016-11-06 11:36:14 +0100

    initial commit

---
+++ greet.cc
@@ -0,0 +1,19 @@
+// ask for a person's name, and greet the person
+#include <iostream>
+#include <string>
+
+using std::cin;             using std::sort<double>;
+
+int main()
+{
+	// ask for the person's name
+	std::cout << "Please enter your first name: ";
+
+	// read the name
+	std::string name;     // define `name'
+	std::cin >> name;     // read into `name'
+
+	// write a greeting
+	std::cout << "Hello, " << name  << "!" << std::endl;
+	return 0;
+}
